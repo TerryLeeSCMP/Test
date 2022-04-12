bbb028277822bdb084aa75cb1ba9aeefeec6afc3
terry lee <terry.lee@scmp.com>
2022-04-12 10:18:37 +0800

    Initial commit

---
+++ Example/Test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by terry lee on 04/12/2022.
+//  Copyright (c) 2022 terry lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
